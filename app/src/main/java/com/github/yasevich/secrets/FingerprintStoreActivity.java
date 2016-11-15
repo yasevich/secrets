e5e26ecdf6fa7444ce83c2b1ca5ad4cb5b0aee60
Slava Yasevich <vyasevich@yamoney.ru>
2016-11-15 19:45:14 +0300

    Implements FingerprintStoreActivity

--- app/src/main/java/com/github/yasevich/secrets/FingerprintStoreActivity.java
+++ app/src/main/java/com/github/yasevich/secrets/FingerprintStoreActivity.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
 import android.widget.EditText;
 
 import com.github.yasevich.secrets.databinding.ActivityFingerprintStoreBinding;
@@ -16,13 +17,20 @@ import com.github.yasevich.secrets.databinding.ViewStoreActionsBinding;
 import com.github.yasevich.secrets.store.FingerprintStore;
 import com.github.yasevich.secrets.store.Store;
 
+import javax.crypto.Cipher;
+
 @TargetApi(Build.VERSION_CODES.M)
 public final class FingerprintStoreActivity extends StoreActivity {
 
     @NonNull
     private final Store store = new FingerprintStore();
 
+    private FingerprintManagerCompat fingerprintManager;
+
     private ActivityFingerprintStoreBinding binding;
+
+    @OperationMode
+    private int operationMode = Cipher.ENCRYPT_MODE;
 
     public static void start(@NonNull Context context) {
         context.startActivity(new Intent(context, FingerprintStoreActivity.class));
@@ -32,6 +40,10 @@ public final class FingerprintStoreActivity extends StoreActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_fingerprint_store);
+
+        fingerprintManager = FingerprintManagerCompat.from(this);
+
+        authenticate();
     }
 
     @NonNull
@@ -42,7 +54,7 @@ public final class FingerprintStoreActivity extends StoreActivity {
 
     @Override
     protected boolean isStoreActionsEnabled() {
-        return false;
+        return fingerprintManager.isHardwareDetected() && fingerprintManager.hasEnrolledFingerprints();
     }
 
     @NonNull
@@ -61,5 +73,41 @@ public final class FingerprintStoreActivity extends StoreActivity {
     @Override
     protected ViewLogBinding getLogBinding() {
         return binding.log;
+    }
+
+    @Override
+    protected void onOperation(@OperationMode int mode) {
+        super.onOperation(mode);
+        operationMode = mode == Cipher.ENCRYPT_MODE ? Cipher.DECRYPT_MODE : Cipher.ENCRYPT_MODE;
+    }
+
+    private void authenticate() {
+        fingerprintManager.authenticate(null, 0, null, new FingerprintManagerCompat.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationFailed() {
+                log("authentication failed");
+            }
+
+            @Override
+            public void onAuthenticationError(int errMsgId, CharSequence errString) {
+                log("authentication error: " + errMsgId + ' ' + errString);
+            }
+
+            @Override
+            public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+                log("authentication help: " + helpMsgId + ' ' + helpString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result) {
+                log("authentication succeeded");
+                if (operationMode == Cipher.ENCRYPT_MODE) {
+                    binding.storeActions.encrypt.callOnClick();
+                } else {
+                    binding.storeActions.decrypt.callOnClick();
+                }
+                authenticate();
+            }
+        }, null);
     }
 }
